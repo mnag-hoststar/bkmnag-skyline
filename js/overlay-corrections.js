5928e57555d7d4bc5687d2f265969aa409ffc567
Eva Jutzeler <eju@hoststar.ch>
2022-06-03 10:57:05 +0200

    #173 Skyline: In the store product view and single product view, the text is too close to the footer.

--- js/overlay-corrections.js
+++ js/overlay-corrections.js
@@ -7,10 +7,12 @@ $( document ).ready(function() {
 	var headerHeight = $(".bk-header").innerHeight();
 	var featureOverlayTopDistance = $( ".widget.feature .widget-overlay" ).attr("style", "top: " + headerHeight +"px");
 	var contentOverlayTopDistance = $( ".bk-pt-default .container" ).attr("style", "top: " + headerHeight +"px");
+	var contentOverlayTopDistance = $( ".bk-pt-default .bk-footer" ).attr("style", "top: " + headerHeight +"px");
 
 	$(window).resize(function() {
 		var headerHeight = $(".bk-header").innerHeight();
 		var featureOverlayTopDistance = $( ".widget.feature .widget-overlay" ).attr("style", "top: " + headerHeight +"px");
 		var contentOverlayTopDistance = $( ".bk-pt-default .container" ).attr("style", "top: " + headerHeight +"px");
+		var contentOverlayTopDistance = $( ".bk-pt-default .bk-footer" ).attr("style", "top: " + headerHeight +"px");
 	});
 });
